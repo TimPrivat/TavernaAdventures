20089b7c1153afdb4e5cf413400686ceeaa7ffd3
Fleischey2 <niklas.gieshuff@gmail.com>
2023-06-23 19:27:23 +0200

    QuestProgress Skript
    Yo

---
+++ TavernaAdventuresTest/Assets/QuestVariables.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestVariables : MonoBehaviour
+{
+
+    public bool firstKey = false;
+    public bool secondKey = false;
+    public bool mainKey = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
